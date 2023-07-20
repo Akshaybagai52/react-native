34f2838becdd76c846f2d662154c55876793f05e
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-20 10:15:07 +0530

    completed image and imagebackground

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,17 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+import logoImg from "./assets/adaptive-icon.png";
 
 export default function App() {
   return (
     <View
       style={{
         display: "flex",
-        alignItems: "center",
-        justifyContent: "space-evenly",
+        
         height: "100%",
         backgroundColor: "plum",
       }}
     >
-      <View
+      
+      <ImageBackground
+        source={{
+          uri: "https://images.unsplash.com/photo-1689326232616-aea5c86f86bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        }}
+        style={{ flex: 1, zIndex: 5}}
+      >
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1689249876759-10ff4bc9f189?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+        }}
+        style={{ height: 200, width: 200 }}
+      />
+      <Image source={logoImg} style={{ height: 200, width: 200 }} />
+
+      </ImageBackground>
+      {/* <View
         style={{ height: 200, width: 159, backgroundColor: "lightgreen" }}
       >
         <Text>Green box</Text>
@@ -20,7 +36,7 @@ export default function App() {
         style={{ height: 200, width: 159, backgroundColor: "lightblue" }}
       >
         <Text>Blue box</Text>
-      </View>
+      </View> */}
     </View>
   );
 }
